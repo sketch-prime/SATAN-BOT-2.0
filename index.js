ce31b4ac828f7a6f363e5ef7e0badf4740e71288
sketch-prime <65995594+sketch-prime@users.noreply.github.com>
2023-04-07 19:51:45 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const { Client, GatewayIntentBits} = require('discord.js');
+require('dotenv/config');
+
+const client = new Client({
+	intents: 131071 
+});
+
+client.on('ready', () =>{
+    console.log('The bot is online!')
+});
+
+client.on('messageCreate', (msg) =>{
+    
+    if(msg.content.toLowerCase() === 'hello')
+    	msg.channel.send('hello there')
+});
+
+client.on('messageCreate', msg => {
+	if(msg.content.toLowerCase() === 'cooking'){
+		client.users.fetch('296386289517723652', false).then((user) => {
+			user.send('Bro now that I know what I am doing I can actually do some pretty fucking cool stuff now LMFAO');
+			msg.channel.send('message sent to ' + user.username)
+   		})
+	}
+});
+
+
+client.login(process.env.TOKEN);
